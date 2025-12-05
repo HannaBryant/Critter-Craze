9d7183d67f76e8258198a461f0c55a80e7822d7c
Hanna Bryant <hannab329@gmail.com>
2025-12-05 02:20:16 +0000

    connected landing page to supabase to record leads

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-function getData() {
+let url = "https://mdhbdrqhokhkpwvajvik.supabase.co/rest/v1/leads";
+let apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kaGJkcnFob2toa3B3dmFqdmlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3OTQyMTcsImV4cCI6MjA4MDM3MDIxN30.5sjiMRuW3e_F17YtaC38XIVXGszjpCEHGsajCiiAzDI";
+
+async function createLead() {
     event.preventDefault();
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let contact = document.getElementById("contact").value;
 
-    let data = {
-        name: name,
-        email: email,
-        contact: contact
+    let lead = {
+
+        "name": name,
+        "email": email,
+        "contact": contact
     };
 
-    console.log(data);
+    let response = await fetch(url, {
+
+        method: "POST",
+        headers: {
+            "apikey": apikey,
+         "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify(lead)
+                                                                                
+    })
 }
