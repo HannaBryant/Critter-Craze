3b7c03c476106586a59f5e7715ccf80aae09f1a8
Hanna Bryant <hannab329@gmail.com>
2025-12-05 03:14:18 +0000

    tweaked form reset in html and js, form now resets properly

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let url = "https://mdhbdrqhokhkpwvajvik.supabase.co/rest/v1/leads";
 let apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kaGJkcnFob2toa3B3dmFqdmlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3OTQyMTcsImV4cCI6MjA4MDM3MDIxN30.5sjiMRuW3e_F17YtaC38XIVXGszjpCEHGsajCiiAzDI";
 
 async function createLead() {
-    event.preventDefault();
+   
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
@@ -28,6 +28,6 @@ async function createLead() {
     });
 
     if (response.ok) {
-        form.reset();
+        document.getElementById("signupForm").reset();
     }
 }
